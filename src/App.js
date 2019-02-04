be3f370b769fcd7e98db5b4b34ae705ee5f30b9a
dorreharay <dorreharay@gmail.com>
2019-02-05 00:00:07 +0200

    added some lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link, Redirect,
 } from 'react-router-dom';
-
-import {
-  Home,
-  Header,
-  SearchPage,
-  DetailsPage,
-} from 'components';
-
 import firebase from './firebase';
+
+import styles from './components/Home/Home.scss';
+
+const Home = (lazy(() => (import('components/Home/Home'))));
+const Header = (lazy(() => (import('components/Header/Header'))));
+const SearchPage = (lazy(() => (import('components/SearchPage/SearchPage'))));
+const DetailsPage = (lazy(() => (import('components/DetailsPage/DetailsPage'))));
 
 class App extends Component {
   state = {
     signed: undefined,
   }
 
-  componentWillMount() {
+  componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({ signed: true });
@@ -30,40 +29,39 @@ class App extends Component {
   render() {
     const { signed } = this.state;
 
+    if (signed === undefined && location.pathname !== '/') {
+      return <div className={styles.main}>Loading...</div>;
+    }
+
     return (
       <div>
-        <Router>
-          <Route render={({ location }) => (
-            <div>
-              {location.pathname === '/' || location.pathname.includes('/search') ? <Header signed={signed} currentLocation={location.pathname} /> : null}
-              {signed !== undefined && !signed && location.pathname !== '/' ? (
-                <Redirect to="/" />
-              ) : (
-                <Switch location={location}>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/search" component={SearchPage} />
-                  <Route exact path="/search/:q" component={SearchPage} />
-                  <Route exact path="/search/details/:username" component={DetailsPage} />
-                  <Route render={() => (
-                    <div style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      flexDirection: 'column',
-                      height: '90vh',
-                    }}
-                    >
-                      <span>Not Found</span>
-                      <Link to="/">Back to home</Link>
-                    </div>
-                  )}
-                  />
-                </Switch>
-              )}
-            </div>
-          )}
-          />
-        </Router>
+        <Suspense fallback={<div className={styles.main}>Loading...</div>}>
+          <Router>
+            <Route render={({ location }) => (
+              <div>
+                {location.pathname === '/' || location.pathname.includes('/search') ? <Header signed={signed} currentLocation={location.pathname} /> : null}
+                {signed !== undefined && !signed && location.pathname !== '/' ? (
+                  <Redirect to="/" />
+                ) : (
+                  <Switch location={location}>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/search" component={SearchPage} />
+                    <Route exact path="/search/:q" component={SearchPage} />
+                    <Route exact path="/search/details/:username" component={DetailsPage} />
+                    <Route render={() => (
+                      <div className={styles.main}>
+                        <span>Not Found</span>
+                        <Link to="/">Back to home</Link>
+                      </div>
+                    )}
+                    />
+                  </Switch>
+                )}
+              </div>
+            )}
+            />
+          </Router>
+        </Suspense>
       </div>
     );
   }
